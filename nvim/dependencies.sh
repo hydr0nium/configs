adc924fa70d59041b5c1af356947045936e2a976
Sol <37932436+hydr0nium@users.noreply.github.com>
2024-05-26 20:58:13 +0200

    Fixed typo

--- nvim/dependencies.sh
+++ nvim/dependencies.sh
@@ -4,9 +4,11 @@ if hash apt 2> /dev/null
 then 
   sudo apt install fd-find ripgrep
   exit
+fi
 
 # User uses pacman package manager
 if hash pacman 2> /dev/null
 then
   sudo pacman -S fd ripgrep
+fi
   
