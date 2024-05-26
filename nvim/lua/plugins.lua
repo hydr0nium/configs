1253e77c5ad3afecf4ced1edf40986b0fe1819ed
Sol <37932436+hydr0nium@users.noreply.github.com>
2024-05-26 21:26:38 +0200

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000
-  },
-  {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
