c0918dbc363399eae7c55ba001483d5866c666a3
Sol <37932436+hydr0nium@users.noreply.github.com>
2024-05-26 21:30:53 +0200

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,7 +1,1 @@
-return {
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
+return {}
