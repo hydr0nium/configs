fa4fbb9528b9bfd78fc69879e53810f57632bfb7
Sol <37932436+hydr0nium@users.noreply.github.com>
2024-05-26 20:48:36 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,7 +23,7 @@ local plugins = {
     priority = 1000
   },
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
