372092e59efa7a6ae4814af6dbf153e16d31e9b8
Sol <37932436+hydr0nium@users.noreply.github.com>
2024-05-26 21:16:26 +0200

    Create plugins.lua

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
