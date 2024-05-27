7e53357f20cddadf1bfb5316802cdf69feaf1359
Sol <37932436+hydr0nium@users.noreply.github.com>
2024-05-27 02:38:15 +0200

    Update dependencies.sh

--- nvim/dependencies.sh
+++ nvim/dependencies.sh
@@ -1,14 +1,22 @@
-
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # User uses apt package manager
 if hash apt 2> /dev/null
 then 
-  sudo apt install fd-find ripgrep
+  sudo apt install fd-find ripgrep npm unzip cmake pkg-config
+  sudo n lts
+  sudo n latest
+  hash -r
   exit
 fi
 
 # User uses pacman package manager
 if hash pacman 2> /dev/null
 then
-  sudo pacman -S fd ripgrep
+  sudo pacman -S fd ripgrep npm nodejs unzip cmake 
+  sudo n lts
+  sudo n latest
+  hash -r
+  exit
 fi
-  
+
+
