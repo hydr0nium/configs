f69bd84332c3e902752a2363b4f75936e85a41a2
Sol <anon@example.com>
2024-05-27 10:11:42 +0000

    Added better completion

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -16,9 +16,10 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.jedi_language_server.setup({})
-			lspconfig.lua_ls.setup({})
+			lspconfig.jedi_language_server.setup({capabilities = capabilities})
+			lspconfig.lua_ls.setup({capabilities = capabilities})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
 		end
